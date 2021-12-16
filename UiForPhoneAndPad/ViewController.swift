4160be9c2134f6b94caa6f6171f7ccdc592ceae5
keshav <keshavrajkashyap711@gmail.com>
2021-12-16 10:59:03 +0530

    Initial Commit

---
+++ UiForPhoneAndPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiForPhoneAndPad
+//
+//  Created by Keshav Raj Kashyap on 16/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
